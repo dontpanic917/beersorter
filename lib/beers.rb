7f56a4fec8a16c79197dbed4839490606aa2ffe3
github username <github email address>
2018-03-06 13:11:03 -0500

    second push

--- lib/beers.rb
+++ lib/beers.rb
@@ -1,2 +1,27 @@
+require 'json'
+require 'pry'
+require 'rest-client'
 class Beers
+  @@all=[]
+  def initialize(name,abv,ibu,srm)
+    @name=name
+    @abv=abv
+    @ibu=ibu
+    @srm=srm
+  end
+  def self.query
+    url="https://api.punkapi.com/v2/beers?"
+    beerlist = RestClient.get(url,{params: {hops: 'cascade'}})
+    # size=beerlist.size
+    # beerlist.map {|beer| @@all << objectizer(beer)}
+    beerarray = JSON(beerlist)
+    beerarraysize = beerarray.size
+    beerarray.map {|beer| @@all << (Beers.new(beer["name"],beer["abv"],beer["ibu"], beer["srm"]))}
+    binding.pry
+  end
+  # def objectizer(beer_hash)
+  #   #imports attributes and instantiates and maps to a beer object using JSON parsing
+  #   @name=  JSON(beerhash)[:name]
+  # end
 end
+Beers.query
--- lib/users.rb
+++ lib/users.rb
@@ -1,2 +1,11 @@
 class Users
+  @@all
+  def initialize(name)
+    @name=name
+    @@all << self
+  end
+  def query(query)
+    beers = get_beers(query)
+    
+  end
 end
